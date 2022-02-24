8b3c94a4eabf3892ccda8bf946a08f94303ff6dc
kenLuckyCapistrano <ken.lucky.capistrano@accenture.com>
2022-02-24 11:09:58 +0800

    find one and find all

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
@@ -7,5 +7,15 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body:createUserDto){
         this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string){
+        return this.userService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string){
+        return this.userService.find(email);
     }
 }
